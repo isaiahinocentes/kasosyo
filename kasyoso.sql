5569ee2579c053c196e56f107cb4271e7eff71d5
Isaiah Paul P. Inocentes <isaiahpaulinocentes@gmail.com>
2017-07-23 00:38:35 +0800

    Add files via upload

--- kasyoso.sql
+++ kasyoso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2017 at 08:23 AM
+-- Generation Time: Jul 22, 2017 at 06:38 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -25,11 +25,31 @@ USE `kasyoso`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `user_id` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`user_id`, `username`, `pass`) VALUES
+('sample', 'sample', 'sample');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `item`
 --
 
 CREATE TABLE `item` (
   `id` varchar(10) NOT NULL,
+  `store_id` varchar(10) NOT NULL,
   `brand` varchar(50) DEFAULT NULL,
   `name_desc` varchar(100) DEFAULT NULL,
   `type` varchar(100) DEFAULT NULL,
@@ -48,8 +68,32 @@ CREATE TABLE `item` (
 -- Dumping data for table `item`
 --
 
-INSERT INTO `item` (`id`, `brand`, `name_desc`, `type`, `quantity`, `unit_qty`, `unit_cap`, `unit_int`, `unit_price`, `bought_date`, `shelf_life`, `instock`, `instock_unit`) VALUES
-('it0', 'nido', 'lactobaciloius milk powder', 'food/dairy', 1, 'kg', 200.4, 20.43, 220.83, '2017-07-22 11:20:36', '2017-11-16', 30, 'kg');
+INSERT INTO `item` (`id`, `store_id`, `brand`, `name_desc`, `type`, `quantity`, `unit_qty`, `unit_cap`, `unit_int`, `unit_price`, `bought_date`, `shelf_life`, `instock`, `instock_unit`) VALUES
+('it0', 'st0', 'nido', 'lactobaciloius milk powder', 'food/dairy', 1, 'kg', 200.4, 20.43, 220.83, '2017-07-22 11:20:36', '2017-11-16', 30, 'kg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `location` varchar(200) DEFAULT NULL,
+  `owner` varchar(100) DEFAULT NULL,
+  `open_hours` varchar(100) DEFAULT NULL,
+  `contact` varchar(100) DEFAULT NULL,
+  `landmark` varchar(100) DEFAULT NULL,
+  `images` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `store`
+--
+
+INSERT INTO `store` (`id`, `name`, `location`, `owner`, `open_hours`, `contact`, `landmark`, `images`) VALUES
+('st0', 'kasyoso store', 'Sta. Mesa, Manila\r\n', 'Manong Pedro', '7:00am - 11:00pm', '096969699', 'Near Rizal park', NULL);
 
 -- --------------------------------------------------------
 
@@ -79,9 +123,21 @@ INSERT INTO `trans` (`id`, `item_id`, `item_qty`, `qty_unit`, `unit_price`, `tot
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `item`
 --
 ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
   ADD PRIMARY KEY (`id`);
 
 --
